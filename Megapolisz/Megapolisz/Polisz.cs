99654395bec372119d3cada238e6bb7f343bf414
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2021-11-15 10:33:26 +0100

    Megapolisz adatbeolvasás

---
+++ Megapolisz/Megapolisz/Polisz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megapolisz
+{
+    public class Polisz
+    {
+        public string Orszag { get; set; }
+        public string Varos { get; set; }
+        public int Nepesseg1 { get; set; }
+        public int Nepesseg2 { get; set; }
+
+        public Polisz(string sor)
+        {
+            var e = sor.Split(';');
+            Orszag = e[0];
+            Varos = e[1];
+            Nepesseg1 = Convert.ToInt32(e[2]);
+            Nepesseg2 = Convert.ToInt32(e[3]);
+        }
+    }
+}
---
+++ Megapolisz/Megapolisz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megapolisz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Polisz> poliszok = new List<Polisz>();
+            try
+            {
+                var sorok = File.ReadAllLines("megapoliszok.txt", Encoding.UTF8);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    poliszok.Add(new Polisz(sorok[i]));
+                }
+                Console.WriteLine("Lista kész!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"3.feladat: Megapoliszok száma:{poliszok.Count} db");
+
+            Console.ReadKey();
+        }
+    }
+}
